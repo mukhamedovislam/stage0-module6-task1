9d83e372bc844e4ba00eaa6be0684629a6f4ca39
mukhamedovislam <bozuy24@gmail.com>
2022-09-26 09:42:13 +0400

    Add Solution

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -11,9 +11,12 @@ public class Animal {
 		this.numberOfPaws = numberOfPaws;
 		this.hasFur = hasFur;
 	}
-
+/*
 	public String getDescription(String color, int numberOfPaws, boolean hasFur){
 		return String.format("This animal is mostly %s. It has %d %s and %s fur.", color, numberOfPaws, numberOfPaws == 1 ? "paw" : "paws", hasFur ? "a" : "no");
 	}
-
+*/
+	public String getDescription(){
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + (numberOfPaws>1?" paws":" paw") + " and " + (hasFur?"a":"no") + " fur.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -4,8 +4,14 @@ public class Bird extends Animal {
 	public Bird(){
 		super("blue", 2, false);
 	}
+/*
 	@Override
 	public String getDescription(String color, int numberOfPaws, boolean hasFur){
 		return String.format("This animal is mostly %s. It has %d %s and %s fur. Moreover, it has 2 wings and can fly.", color, numberOfPaws, numberOfPaws == 1 ? "paw" : "paws", hasFur ? "a" : "no");
 	}
+*/
+	@Override
+    public String getDescription() {
+        return "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
+    }
 }
